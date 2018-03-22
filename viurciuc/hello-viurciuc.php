4a2a5a98a655c3d44b4fc72d5110f50786ae2e73
Vladislav Iurciuc <fletcher6847@gmail.com>
2018-03-22 10:33:25 +0200

    Improved code

--- viurciuc/hello-viurciuc.php
+++ viurciuc/hello-viurciuc.php
@@ -1,35 +1,24 @@
 <?php
 
-if ($_SERVER['argc'] % 2 == 0) {
-	echo 'Missing arguments.';
-	exit(0);
+if ($_SERVER['argc'] % 2 == 0 || $_SERVER['argc'] == 1) {
+	die('Missing arguments.');
 }
 
-if ($_SERVER['argc'] == 1) {
-	echo 'Missing arguments.';
-	exit(0); 
-}
-
-for ($i = 1; $i < $_SERVER['argc']; $i += 2) {
-	if ($_SERVER['argv'][$i] != 'm' && $_SERVER['argv'][$i] != 'M' && $_SERVER['argv'][$i] != 'f' && $_SERVER['argv'][$i] != 'F') {
-		echo "Unknown title.";
-		exit(0);
-	}
-}
-
-echo 'Hello ';
+$result = 'Hello ';
 
 for ($i = 1; $i <= $_SERVER['argc'] - 1; $i += 2) {
 	
 	if ($_SERVER['argv'][$i] == 'm' || $_SERVER['argv'][$i] == 'M') {
-		echo 'Mr '. \ucfirst(\strtolower($_SERVER['argv'][$i + 1]));
+		$result .= 'Mr '. \ucfirst(\strtolower($_SERVER['argv'][$i + 1]));
 	} elseif ($_SERVER['argv'][$i] == 'f' || $_SERVER['argv'][$i] == 'F') {
-		echo 'Ms '. \ucfirst(\strtolower($_SERVER['argv'][$i + 1]));
+		$result .= 'Ms '. \ucfirst(\strtolower($_SERVER['argv'][$i + 1]));
+	} else {
+		die("Unknown title.");
 	}
 
 	if ($i < $_SERVER['argc'] - 2) {
-		echo ' and ';
+		$result .= ' and ';
 	}
 }
 
-echo "!\n";
+echo $result . "!\n";
