6605954e2ed911fec3ceff2225aa14acf00eb277
Dima Carmanov <dima@dima.com>
2018-03-23 12:57:35 +0200

    Hello Homework

---
+++ hello/hello-dcarmanov.php
@@ -0,0 +1,39 @@
+<?php
+
+$b = $_SERVER['argv'];
+
+$array_count = count($argv);
+
+if ($array_count%2 !== 1 || $array_count == 1) die("Missing arguments" . PHP_EOL);
+
+echo "Hello ";
+
+for ($i=1; $i<$array_count-1; $i+=2) {
+
+    $j=$i;
+
+    if ($argv[$i] == 'm' || $argv[$i] == 'M') {
+
+        $sex = 'Mr.';
+    }
+    elseif ($argv[$i] == 'f' || $argv[$i] == 'F') {
+
+        $sex = 'Ms.';
+    }
+    else echo die ("Unknown title!" . PHP_EOL);
+
+    if ($array_count>3 && $i < $array_count-2 ){
+
+        $and = " and ";
+
+    } else $and = "";
+
+
+    echo $sex;
+
+    echo ucfirst($argv[$j+1]);
+
+    echo $and;
+}
+
+echo "!" . PHP_EOL;
