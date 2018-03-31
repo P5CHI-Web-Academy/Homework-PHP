217b11e88122c38eb5e93d3f2a2f67133ce545c5
Vadim M <13vadimm@gmail.com>
2018-03-31 17:34:09 +0300

    Up 0000

---
+++ jsondb/count-vmasulis.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER['argc'] !== 2) {
+    exit('Only one argument is accepted.');
+}
+$GLOBALS['owner'] = $_SERVER['argv'][1];
+$GLOBALS['path'] = "report".DIRECTORY_SEPARATOR.$owner;
+
+if (!is_dir($path)) {
+	mkdir($path, 0777, true);
+}
+
+$owner_data = [];
+try {
+	$db = new \PDO('sqlite:mess.db');
+	if (!$db) exit("Could not open database!");
+	$st = $db->prepare('SELECT COUNT(id) AS `count`, type FROM info WHERE owner = :owner GROUP BY type');
+	$st->execute([':owner' => $owner]);
+	$res = $st->fetchAll();
+	if (count($res) <= 0) {
+		exit('No information.');
+	}
+	foreach ($res as $row) {
+		$row['type'] = strtolower($row['type']);
+		$owner_data[$row['type']] = $row['count'];
+	}
+} catch (PDOException $e) {
+	exit('PDOException are detected: '.$e->getMessage());
+}
+file_put_contents($path.DIRECTORY_SEPARATOR.'count.json', json_encode($owner_data));
