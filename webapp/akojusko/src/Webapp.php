a9d1e077921892dfd9c430783f012f80ace8a3a8
Andrei Kojusko <akojusko@pentalog.fr>
2018-04-02 12:21:41 +0300

    add example of template processing

--- webapp/akojusko/src/Webapp.php
+++ webapp/akojusko/src/Webapp.php
@@ -10,4 +10,19 @@ class Webapp
 
         return true;
     }
+
+    //example of template rendering
+    public function renderTemplate(string $fileName, array $vars = []): void
+    {
+        //1. load template contents as string
+        $content = \file_get_contents($fileName);
+
+        //2. replace {{ varname }} placeholders with real data
+        foreach ($vars as $key => $value) {
+            $content = str_replace('{{ '.$key.' }}', $value, $content);
+        }
+
+        //3. output processed template
+        echo($content);
+    }
 }
