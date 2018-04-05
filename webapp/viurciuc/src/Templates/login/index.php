03d78ec25ff352774a87541e90a5fa2b76893b64
Vladislav Iurciuc <fletcher6847@gmail.com>
2018-04-05 15:33:25 +0300

    without tests

--- webapp/viurciuc/src/Templates/login/index.php
+++ webapp/viurciuc/src/Templates/login/index.php
@@ -19,9 +19,9 @@
 <form class="form-signin" method="POST" action="/login">
     <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
     <label for="inputEmail" class="sr-only">Email address</label>
-    <input type="text" id="inputEmail" name="username" class="form-control" placeholder="test_user" autofocus>
+    <input type="text" id="inputEmail" name="username" class="form-control" placeholder="Vlad6847" autofocus>
     <label for="inputPassword" class="sr-only">Password</label>
-    <input type="password" id="inputPassword" name="password" class="form-control" placeholder="test_password">
+    <input type="password" id="inputPassword" name="password" class="form-control" placeholder="12345">
 
     <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
 
