cb117c2c44f1135e457d6e38a47169479213fe6c
Vladislav Iurciuc <fletcher6847@gmail.com>
2018-04-05 20:53:39 +0300

    hw

--- webapp/viurciuc/src/Controller/LoginController.php
+++ webapp/viurciuc/src/Controller/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Services\API\GitInterface;
 use App\Services\AuthService;
-
+use App\Templates;
 
 class LoginController
 {
@@ -25,27 +25,55 @@ class LoginController
     private $gitService;
 
     /**
-     * @param AuthService $auth
+     * @param AuthService  $auth
      * @param GitInterface $gitService
      */
     public function __construct(AuthService $auth, GitInterface $gitService)
     {
-        $this->auth = $auth;
+        $this->auth       = $auth;
         $this->gitService = $gitService;
     }
 
-    public function index()
+    public function index(): void
     {
         if ($this->auth->isAuthenticated()) {
-            $_SESSION['userGitLink'] =
-                $_SESSION['userGitLink'] ?? $this->gitService->getProfileLink($_SESSION['username']);
+            $_SESSION['userGitLink'] = $_SESSION['userGitLink'] ??
+                                       $this->gitService->getProfileLink(
+                                           $_SESSION['username']
+                                       );
 
-            include $this->templatePath . 'main/index.php';
+            Templates\TemplateEngine::render(
+                $this->templatePath.'main/index.php',
+                [
+                    'username' => $_SESSION['username'] ?? '',
+                    'GitLink'  => $_SESSION['userGitLink'],
+                ]
+            );
+
+            //include $this->templatePath . 'main/index.php';
 
             return;
         }
 
-        include $this->templatePath . 'login/index.php';
+        if (isset($_SESSION['error'])) {
+            Templates\TemplateEngine::render(
+                $this->templatePath.'login/index.php',
+                [
+                    'error' => '<div class="alert alert-danger" role="alert">'
+                               .$_SESSION['error'].'</div>',
+                ]
+            );
+            unset($_SESSION['error']);
+        } else {
+            Templates\TemplateEngine::render(
+                $this->templatePath.'login/index.php',
+                [
+                    'error' => '',
+                ]
+            );
+
+        }
+        //include $this->templatePath.'login/index.php';
     }
 
     public function login(array $request = [])
@@ -54,7 +82,7 @@ class LoginController
             $this->redirect($_SERVER['HTTP_HOST']);
         }
 
-        if (! $this->auth->authenticate($request)) {
+        if ( ! $this->auth->authenticate($request)) {
             $_SESSION['error'] = 'Username and/or password is invalid';
 
             $this->redirect($_SERVER['HTTP_HOST']);
